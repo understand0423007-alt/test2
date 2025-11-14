7981a272daaff4aed2267fe80a4d052a323e78e1
seike <understand0423007@gmail.com>
2025-11-14 15:55:29 +0900

    Add test_project folder

---
+++ test_project/add.js
@@ -0,0 +1,6 @@
+let name ="太郎";
+let age = 25;
+let height = 170.5;
+let is_student= false;
+
+console.log('名前: ${name},年齢: &{age},身長: ${height} cm,学生か？ ${is_student}');
---
+++ test_project/add.py
@@ -0,0 +1,6 @@
+name = "太郎"
+age = 25
+height = 170.5
+is_student = False
+
+print(f"名前: {name}, 年齢: {age}, 身長: {height} cm, 学生か？ {is_student}")
